ee618dcd90c8037b509f2d727b65c851431c8e40
TuqaMohamed <toka.m.mahdy@gmail.com>
2023-12-22 16:50:20 +0200

    after adding the env variables

--- clinic-appointment-app/src/app/doctor/doctor.service.ts
+++ clinic-appointment-app/src/app/doctor/doctor.service.ts
@@ -2,21 +2,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable ,tap} from 'rxjs';
+import { environment } from 'src/environments/environment';
+import Config from "src/config.json";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
-  private apiUrl = 'http://localhost:8080'; 
+  private apiUrl=Config.API_URL;
 
   constructor(private http: HttpClient) { }
 
   getDoctors(): Observable<any[]> {
+    console.log('Constructed URL:', `${this.apiUrl}/signup`);
     return this.http.get<any[]>(`${this.apiUrl}/getslots`);
   }
 
   getDoctorSlots(doctorId: string): Observable<any[]> {
     const url = `${this.apiUrl}/getdrslot/${doctorId}/slots`;
+    console.log('Constructed URL:', `${this.apiUrl}/signup`);
     return this.http.get<any[]>(url);
   }
 
@@ -24,11 +28,13 @@ export class DoctorService {
     const url = `${this.apiUrl}/insertslot/${doctorId}`;  
     console.log('Request payload:', slot);
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    console.log('Constructed URL:', `${this.apiUrl}/signup`);
     return this.http.post<any>(url, slot);
   }
 
   getSlotId(date: string, time: string, doctorName: string): Observable<any> {
     const url = `${this.apiUrl}/slotid/${date}/${time}/${doctorName}`;
+    console.log('Constructed URL:', `${this.apiUrl}/signup`);
     return this.http.get(url);
   }
   
--- clinic-appointment-app/src/app/login/login.service.ts
+++ clinic-appointment-app/src/app/login/login.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders ,HttpErrorResponse} from '@angular/common/http';
 import { Observable ,tap} from 'rxjs';
+import { environment } from 'src/environments/environment';
+import Config from "src/config.json";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:8080';
+  private apiUrl=Config.API_URL;
 
   constructor(private http: HttpClient) {}
 
@@ -14,7 +16,7 @@ export class LoginService {
     const user = { Email, Password };
     console.log('Request payload:', user);
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
+    console.log('Constructed URL:', `${this.apiUrl}/signup`);
     return this.http.post(`${this.apiUrl}/signin`, user);
   }
 }
